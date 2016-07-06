d2ec97972cd2c64170cdfa2a09f817bedca688ba
xiongtoto0526 <xiongtoto0526@163.com>
2016-07-06 16:14:05 +0800

    add tether

---
+++ angularJS/ng-html-compile/x.js
@@ -0,0 +1,33 @@
+
+var app = angular.module('xApp', []);
+
+app.controller('xCtrl', ['$scope', function ($scope) {
+   $scope.text = 'x';
+   // $scope.xhtHtml = 'htllo';
+}])
+
+
+app.directive('ngHtmlCompile',function ($compile) {
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+          scope.$watch(
+            function(scope) {
+               // watch the 'compile' expression for changes
+              return scope.$eval(attrs.ngHtmlCompile);
+            },
+            function(value) {
+              // when the 'compile' expression changes
+              // assign it into the current DOM
+              element.html(value);
+
+              // compile the new DOM and link it to the current
+              // scope.
+              // NOTE: we only compile .childNodes so that
+              // we don't get into infinite loop compiling ourselves
+              $compile(element.contents())(scope);
+            }
+        );
+    }
+      };
+});
